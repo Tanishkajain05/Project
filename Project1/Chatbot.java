65aae1f2a9d804e2a9d6bf40d935480aaa177938
Tanishkajain05 <jaintanishka527@gmail.com>
2025-04-20 21:41:12 +0530

    updated

--- Project1/Chatbot.java
+++ Project1/Chatbot.java
@@ -1,7 +1,7 @@
 import java.util.*;
  public class Chatbot {
     public static void main(String args[]){
-        System.out.println("tanishka and aditya");
+        System.out.println("tanishka");
     }
  }
  
