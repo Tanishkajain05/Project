67d70c5395d187af628715fd9284fc66a4cf8ca7
Tanishkajain05 <jaintanishka527@gmail.com>
2025-04-21 16:32:24 +0530

    21 april 2025

--- Project1/Chatbot.java
+++ Project1/Chatbot.java
@@ -3,6 +3,16 @@ import java.util.*;
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String a = sc.nextLine();
+        String input;
+        System.out.println("Chatbot is activated.. type bye to end the chat\n");
+        while (true) {
+            System.out.println("you: ");
+            input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Chatbot: Goodbye! Take care :)");
+                break;
+            }
+        }
         if (a.equalsIgnoreCase ("hey") || 
          a.equalsIgnoreCase("hii") || 
          a.equalsIgnoreCase("Hey") ||
@@ -15,22 +25,23 @@ import java.util.*;
         } else if
             (a.equalsIgnoreCase("i am fine") ||
              a.equalsIgnoreCase("i am fine."))
-             System.out.println("ohh! It's good to know that you are fine. im' fine too thank you for asking me..\n would you like me to tell you a joke?");
+             System.out.println("Chatbot: ohh! It's good to know that you are fine. im' fine too thank you for asking me..\n would you like me to tell you a joke?");
            else if 
            (a.equalsIgnoreCase("i'm fine, how are you?")) {
-                System.out.println("ohh! It's good to know that you are fine. im' fine too thank you for asking me..\n would you like me to tell you a joke?");
+                System.out.println("Chatbot: ohh! It's good to know that you are fine. im' fine too thank you for asking me..\n would you like me to tell you a joke?");
             } else if 
                 (a.equalsIgnoreCase("yes tell me a joke") ||
                 a.equalsIgnoreCase("i want to listen a joke")) {
-                    System.out.println("Yeah sure.. Here it is.\n1.Student: Can I go to the bathroom?\nTeacher: It is May\nstudent: No, it is April, but I still gotta go.\n2. I told my computer I needed a break.\n Now it will not stop sending me KitKat ads.\n");
+                    System.out.println("Chatbot: Yeah sure.. Here it is.\n1.Student: Can I go to the bathroom?\nTeacher: It is May\nstudent: No, it is April, but I still gotta go.\n2. I told my computer I needed a break.\n Now it will not stop sending me KitKat ads.\n");
                 }
             
              else if (a.equalsIgnoreCase("no.. i am not fine i am sad.") ||
             a.equalsIgnoreCase("i am not feeling well")||
             a.equalsIgnoreCase("my mood is too off") ||
             a.equalsIgnoreCase("i am not fine")) {
-                System.out.println("ohh dear! no problem \n i am here to lighten up your mood \nlet me tell you some jokes \n here it is.\n1. Student: Can I go to the bathroom? \nTeacher: It is May.\n Student: No, it is April, but I still gotta go.\n2.I told my computer I needed a break.\nNow it will not stop sending me KitKat ads.\n");
+                System.out.println("Chatbot: ohh dear! no problem \n i am here to lighten up your mood \nlet me tell you some jokes \n here it is.\n1. Student: Can I go to the bathroom? \nTeacher: It is May.\n Student: No, it is April, but I still gotta go.\n2.I told my computer I needed a break.\nNow it will not stop sending me KitKat ads.\n");
             }
+          
         
     }
 } 
